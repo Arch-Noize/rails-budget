f5acbcbd00e6089f079ed309a448f109feb73db8
Arch-Noize <santiagonoize@gmail.com>
2024-01-09 05:35:57 -0400

    Add devise, and other gems

--- db/migrate/20240109091109_create_purchases.rb
+++ db/migrate/20240109091109_create_purchases.rb
@@ -2,7 +2,8 @@ class CreatePurchases < ActiveRecord::Migration[7.1]
   def change
     create_table :purchases do |t|
       t.text :name
-      t.decimal :amount
+      t.decimal :amount, default: 0.00
+      t.references :author, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
     end
