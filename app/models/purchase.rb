a4c595ac6990db728c200a8df80f29d1fdbe8ebd
Arch-Noize <santiagonoize@gmail.com>
2024-01-09 10:33:03 -0400

    Add foreign key

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,8 @@
 class Purchase < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, foreign_key: "author_id"
     has_many :groups
 
     validates :name, presence: true
     validates :amount, presence: true, numericality: {greater_than_or_equal_to: 0.01}
+    validates :author_id, presence: true
 end
