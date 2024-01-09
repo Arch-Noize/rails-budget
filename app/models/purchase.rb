163571c6b1752ffc0bd83375b69e65f1eb409466
Arch-Noize <santiagonoize@gmail.com>
2024-01-09 09:53:35 -0400

    Add model validations and relations

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,7 @@
 class Purchase < ApplicationRecord
+    belongs_to :user
+    has_many :groups
+
+    validates :name, presence: true
+    validates :amount, presence: true, numericality: {greater_than_or_equal_to: 0.01}
 end
