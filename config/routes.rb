4561dcc164fc6ee5869aeb91c0eb647bcf44db75
Arch-Noize <santiagonoize@gmail.com>
2024-01-10 08:53:08 -0400

    Fix/organize navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  root to: "users#home"
+  root to: "pages#home"
 
   devise_for :users
 
-  resources :users, only: [:home]
   resources :groups, only: [:index, :show, :new], path: "categories" do
     resources :purchases, only: [:index, :show, :new], :path => "purchases"
   end
