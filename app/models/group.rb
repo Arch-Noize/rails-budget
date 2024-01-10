4755880ba589bf3207611b17c17671688e8aec42
Arch-Noize <santiagonoize@gmail.com>
2024-01-10 08:24:22 -0400

    Update models and add join table

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,13 @@
 class Group < ApplicationRecord
-    belongs_to :user
-    has_many :purchases
+    belongs_to :author, class_name: "User"
+    has_many :group_purchases, dependent: :destroy
+    has_many :purchases, through: :group_purchases, dependent: :destroy
 
     validates :name, presence: true
     validates :icon, presence: true
+
+    # Returns the total amount of all purchases in the group
+    def total
+        purchases.sum(:amount)
+    end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,7 @@
 class Purchase < ApplicationRecord
     belongs_to :user, foreign_key: "author_id"
-    has_many :groups
+    has_many :group_purchases, dependent: :destroy
+    has_many :groups, through: :group_purchases, dependent: :destroy
 
     validates :name, presence: true
     validates :amount, presence: true, numericality: {greater_than_or_equal_to: 0.01}
