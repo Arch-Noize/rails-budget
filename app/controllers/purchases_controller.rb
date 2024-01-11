911c3547a9df5238061a2156a1ac57927c349cbd
Arch-Noize <santiagonoize@gmail.com>
2024-01-11 11:17:56 -0400

    Add Purchase methods

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,32 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_group
 
   def index
-    @user = User.includes(:purchases).find(params[:author_id])
-    @purchases = @user.purchases
+    @purchases = @group.purchases.order(created_at: :desc)
+    @total_amount = @group.total
   end
-
-  def show; end
 
   def new
     @user = current_user
     @purchase = @user.purchases.build
   end
 
+  def create
+    @purchase = @group.purchases.new(purchase_params)
+    @purchase.author_id = current_user.id
+    if @purchase.save
+      redirect_to group_purchases_path(@group), notice: 'Purchase was successfully added.'
+    else
+      render :new
+    end
+  end
+
   private
+
+  def set_group
+    @group = current_user.groups.includes(:purchases).find(params[:group_id])
+  end
 
   def purchase_params
     params.require(:purchase).permit(:name, :amount)
