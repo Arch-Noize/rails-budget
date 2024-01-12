f76c78f7f85eec3ab1fa41ff3609f18546653294
Arch-Noize <santiagonoize@gmail.com>
2024-01-12 14:32:54 -0400

    Add splash screen tests

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'Testing , it', type: :feature do
-  before(:each) do
-    
+RSpec.describe 'Testing Pages#root, it should', type: :feature do
+  before :each do
+    visit root_path
   end
 
+  it 'has the log in button' do
+    expect(page).to have_content('LOG IN')
+  end
+
+  it ' has the sign up ' do
+    expect(page).to have_content('SIGN UP')
+  end
 end
